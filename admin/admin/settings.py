6c9c8d4817e57fedec67372af8970e6481f85c43
AdityaKaran109 <AdityaKaran109@gmail.com>
2023-03-03 20:07:45 +0530

    added docker working

--- admin/admin/settings.py
+++ admin/admin/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-$@_2f0ysddzz+yw^a6zx!cx9k$!kovvqi(*$@4tj8ac+5pw9-q
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0']
 
 
 # Application definition
